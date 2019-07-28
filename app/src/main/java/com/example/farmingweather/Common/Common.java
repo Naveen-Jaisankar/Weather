3211979b7f98559cfa491178286b41a59e604050
Naveen <naveen.jaisankar1999@gmail.com>
2019-07-28 17:42:19 +0530

    Weather

---
+++ app/src/main/java/com/example/farmingweather/Common/Common.java
@@ -0,0 +1,10 @@
+package com.example.farmingweather.Common;
+
+import android.location.Location;
+
+public class Common {
+     public static  final String APP_ID="63dcae0c8867434cd59f1b51cbfcf267";
+     public static Location current_location=null;
+
+
+}
---
+++ app/src/main/java/com/example/farmingweather/Model/Clouds.java
@@ -0,0 +1,19 @@
+package com.example.farmingweather.Model;
+
+public class Clouds {
+
+
+    private int all;
+
+    public Clouds() {
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
+
